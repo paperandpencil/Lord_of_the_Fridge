6b696d9e32e6810b130542feb05ae1278fd30d81
paperandpencil <sujunjie@gmail.com>
2026-02-09 23:56:44 +0800

    feat: added a function, is_vallid_image() to verify status of uploaded file

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import (
     redirect,
 )
 from werkzeug.utils import secure_filename
+from PIL import Image
 
 app = Flask(__name__)
 app.config["TEST_SECRET"] = os.environ.get("TEST_SECRET")
@@ -26,12 +27,28 @@ def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def is_valid_image(filename):
+    try:
+        img = Image.open(filename)
+        img.verify()
+        img.close()
+        with Image.open(filename) as img:
+            img.load()
+        return True
+    except (IOError, SyntaxError, FileNotFoundError) as e:
+        print(f"Invalid image or file error: {e}")
+        return False
+    except Exception as e:
+        print(f"Unexpected error occurred: {e}")
+        return False
+
+
 @app.route("/", methods=["GET", "POST"])
 # v1 / sanity check
 # def hello_world():
 # 	return "<p>Hello, World!</p>"
 def index():
-    print(f"The secret key is: {app.config['TEST_SECRET']}")  # testing
+    # print(f"The secret key is: {app.config['TEST_SECRET']}")  # testing
 
     if request.method == "POST":
         if "file" not in request.files:
@@ -39,12 +56,15 @@ def index():
         file = request.files["file"]
         if file.filename == "":
             return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # sanitize!
+        if is_valid_image(file.filename):  # else skip (& do nothing, silently...)
+            filename = secure_filename(file.filename)  # sanitize!, temporary situation
+            # todo, rename filename to timestamp_42intraLogin
+            # answers 2 questions:
+            # (1) when was the item was deposited into the fridge, (2) by who
             file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
             return redirect(url_for("index"))
 
-    # list images for gallery
+    # if GET, then just list images for gallery
     image_files = os.listdir(UPLOAD_FOLDER)  # list contents in UPLOAD_FOLDER
     images = [f for f in image_files if f.endswith((".png", ".jpg", ".jpeg", ".gif"))]
     return render_template("gallery.html", images=images)
